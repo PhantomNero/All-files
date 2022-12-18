9ec1c943d3cd7ede77063b7b1c7f21dc0a951c66
PhantomNero <116944623+PhantomNero@users.noreply.github.com>
2022-12-18 18:05:06 +0300

    18.12.2022

--- Ping-Pong/Ping-Pong.py
+++ Ping-Pong/Ping-Pong.py
@@ -1,10 +1,5 @@
 from pygame import *
-
-"""Main window."""
-win_width = 700
-win_height = 500
-display.set_caption("Ping-Pong")
-window = display.set_mode((win_width, win_height))
+from time import *
 
 """Classes."""
 class GameSprite(sprite.Sprite):
@@ -36,5 +31,45 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
+
+"""Main window."""
+back = (246, 187, 131)
+win_width = 600
+win_height = 500
+display.set_caption("Ping-Pong")
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+"""Flags."""
+game = True
+finish = False
+#clock = time.Clock()
+FPS = 60
+
+"""Ball and rackets for game."""
+racket1 = Player("racket.png", 30, 200, 4, 50, 150)
+racket2 = Player("racket.png", 520, 200, 4, 50, 150)
+ball = GameSprite("tenis_ball.png", 200, 200, 4 , 50, 50)
+
+"Fonts."
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render("Player 1 lost.", True, (180, 0, 0))
+lose2 = font.render("Player 2 lost.", True, (180, 0, 0))
+
+"""Cycle."""
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_l()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
 
